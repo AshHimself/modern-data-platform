63d02d87bc60c75c0dbd7799dbeb11ab9b8a37bc
Ash Smith <missenlinx@gmail.com>
2021-07-04 16:13:28 +0800

    Create bootstrap_postgres.sql

---
+++ bootstrap_postgres.sql
@@ -0,0 +1,3 @@
+CREATE SCHEMA raw; --store all raw data here
+
+CREATE SCHEMA analytics; --Everything here will be owned by DBT; this is essentially our warehouse where all data marts will be created.
